c26b8193b43e5cd70c6683c02d0d6f8a41512593
Sali-one <168825039+Sali-one@users.noreply.github.com>
2024-12-23 16:03:46 +0330

    new version of 2 file

---
+++ Game.cpp
@@ -0,0 +1,91 @@
+#include "Game.h"
+#include <iostream>
+using namespace std;
+const string Path = "resources/";
+const string Err ="Error loading";
+
+
+vector<Line> lines;
+Game::Game()
+    :windowa(sf::VideoMode(1080,800), "Sheep Fight"),
+      player1(1), player2(2), isGameOver(false) {
+
+    if (!font.loadFromFile(Path+"arial.ttf")) {
+        std::cerr << Err + " font!" << std::endl;
+    }
+
+    
+}
+
+void Game::run() {
+    sf::Clock clock;
+
+    sf::Texture backgroundTexture;
+    if (!backgroundTexture.loadFromFile(Path +"background.jpg")) {
+        std::cerr << Err + "background image!" << std::endl;
+    }
+    sf::Sprite backgroundSprite(backgroundTexture);
+
+    sf::Texture arrowTexture;
+    if (!arrowTexture.loadFromFile(Path+"arrow.png")) {
+        std::cerr << Err+" arrow image!" << std::endl;
+    }
+    sf::Sprite arrowSprite(arrowTexture);
+
+    for(int i=0 ; i<5 ; i++){
+        Line line1(i);
+        line1.renderL(windowa);
+        lines.push_back(line1);
+    }
+
+    
+    while (windowa.isOpen()) {
+        sf::Time deltaTime = clock.restart();
+        processEvents();
+        if (!isGameOver) {
+            update(deltaTime);
+        }
+        render(backgroundSprite, arrowSprite);
+    }
+}
+
+
+void Game::processEvents() {
+    sf::Event event;
+    while (windowa.pollEvent(event)) {
+        if (event.type == sf::Event::Closed) {
+            windowa.close();
+        }
+
+        player1.handleInput(windowa, event,  1);
+        player2.handleInput(windowa, event,  2);
+    }
+}
+
+void Game::update(sf::Time deltaTime) {
+    for (auto& line : lines) {
+        line.update(deltaTime);
+    }
+
+    if (player1.getHealth() <= 0 || player2.getHealth() <= 0) {
+        isGameOver = true;
+    }
+}
+
+void Game::render(sf::Sprite& backgroundSprite, sf::Sprite& arrowSprite) {
+    windowa.clear();
+    
+    windowa.draw(backgroundSprite);
+    
+    
+    
+    for ( auto& line : lines) {
+        line.renderL(windowa);
+    }
+
+    player1.render(windowa, font, true, arrowSprite);
+    player2.render(windowa, font, false, arrowSprite);
+
+    windowa.display();
+}
+
---
+++ Sheep.cpp
@@ -0,0 +1,74 @@
+#include "Sheep.h"
+#include <iostream>
+using namespace std;
+
+const string Err ="Error loading";
+const string Path = "resources/";
+Sheep::Sheep(int type) : type(type) {
+
+    if (type == 0) { 
+        power = 10; 
+        damage = 5; 
+        health = 50;
+        speed = 100; 
+        if (!texture.loadFromFile(Path+"small_sheep.png")) {
+            std::cerr << Err+" small_sheep.png" << std::endl;
+        }
+    } else if (type == 1) {
+        power = 20;
+        damage = 10; 
+        health = 75;
+        speed = 75;  
+        if (!texture.loadFromFile(Path+"medium_sheep.png")) {
+            std::cerr << Err +" medium_sheep.png" << std::endl;
+        }
+    } else if (type == 2) {
+        power = 30; 
+        damage = 20; 
+        health = 100; 
+        speed = 50;  
+        if (!texture.loadFromFile(Path+"heavy_sheep.png")) {
+            std::cerr << Err+" heavy_sheep.png" << std::endl;
+        }
+    }
+
+    sprite.setTexture(texture);
+}
+
+sf::Vector2f Sheep::getPosition() const {
+    return sprite.getPosition();
+}
+
+void Sheep::setPosition(sf::Vector2f position) {
+    sprite.setPosition(position);
+}
+
+void Sheep::move(sf::Vector2f offset) {
+    sprite.move(offset);
+}
+
+float Sheep::getSpeed() const {
+    return speed;
+}
+
+int Sheep::getPower() const {
+    return power;
+}
+
+int Sheep::getDamage() const {
+    return damage;
+}
+
+void Sheep::takeDamage(int amount) {
+    health -= amount;
+}
+
+bool Sheep::isDead() const {
+    return health <= 0;
+}
+
+void Sheep::render(sf::RenderWindow& window, sf::Vector2f position) {
+    sprite.setPosition(position);
+    window.draw(sprite);
+}
+
